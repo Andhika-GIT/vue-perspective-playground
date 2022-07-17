f0bfc37a0cbaf8e7c4f0beebce3c73730aa0be58
Andhika Prasetya <andhikapr081@gmail.com>
2022-07-17 13:33:16 +0700

    make perspective works setup the rotatex,y,z

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      perspective: 100,
+      perspective: 0,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
     };
   },
   methods: {
-    setPerspective(e) {
-      console.log(e.target.value);
-      //   this.perspective = e.target.value;
+    onReset() {
+      this.perspective = 0;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
     },
   },
   computed: {
     setPerspective() {
-      return { perspective: this.perspective + 'px' };
+      return {
+        // perspective: `${this.perspective}px`,
+        transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)`,
+      };
     },
   },
-}).mount('#app');
+}).mount("#app");
