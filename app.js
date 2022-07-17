c59e20b55774ef5bd1bc543961332b5a7b2b6017
Andhika Prasetya <andhikapr081@gmail.com>
2022-07-17 13:42:34 +0700

    little fix for reset

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      perspective: 0,
+      perspective: 100,
       rotateX: 0,
       rotateY: 0,
       rotateZ: 0,
@@ -11,7 +11,7 @@ createApp({
   },
   methods: {
     onReset() {
-      this.perspective = 0;
+      this.perspective = 100;
       this.rotateX = 0;
       this.rotateY = 0;
       this.rotateZ = 0;
@@ -20,7 +20,6 @@ createApp({
   computed: {
     setPerspective() {
       return {
-        // perspective: `${this.perspective}px`,
         transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)`,
       };
     },
